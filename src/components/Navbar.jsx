d0cbbf326e0062dd22b865f4ab662f562f353a47
Sukhveer <techforindia03@gmail.com>
2025-04-16 04:49:36 +0530

    Navbar login/register button fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
   return (
         <nav>
               <Link to="/" className="routeLink">Home</Link>
-              <Link to="/login" className="routeLink">Login</Link>
-              <Link to="/register" className="routeLink">Register</Link>
+              {/* <Link to="/login" className="routeLink">Login</Link> */}
+              {/* <Link to="/register" className="routeLink">Register</Link> */}
       <Link to="/dashboard" className="routeLink">Dashboard</Link>
       <Link to="/profile" className="routeLink">Profile</Link>
       <Link to="/admindashboard" className="routeLink">Admin Dashboard</Link>
